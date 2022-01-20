311d62b025fceedb6ab3aff939fe40f30d49b9e8
Hugo Costa <costarafaelhugo@gmail.com>
2022-01-20 11:21:24 -0300

    Fiz a primeira versão do layout

--- app/src/main/java/com/hugorafaelcosta/chucknorrisjokeapp/MainActivity.kt
+++ app/src/main/java/com/hugorafaelcosta/chucknorrisjokeapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.hugorafaelcosta.chucknorrisjokeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
     }
+
 }
--- app/src/main/java/com/hugorafaelcosta/chucknorrisjokeapp/SplashScreen.kt
+++ app/src/main/java/com/hugorafaelcosta/chucknorrisjokeapp/SplashScreen.kt
@@ -3,6 +3,7 @@ package com.hugorafaelcosta.chucknorrisjokeapp
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
