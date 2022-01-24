9b50e79ed3cb488fdc19bc72442e26186a0360d7
Hugo Costa <costarafaelhugo@gmail.com>
2022-01-24 13:35:42 -0300

    Finalizei a parte lógica e api

--- app/src/main/java/com/hugorafaelcosta/chucknorrisjokeapp/MainActivity.kt
+++ app/src/main/java/com/hugorafaelcosta/chucknorrisjokeapp/MainActivity.kt
@@ -1,16 +1,54 @@
 package com.hugorafaelcosta.chucknorrisjokeapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Website.URL
+import android.text.Html
 import android.view.View
-import android.view.Window
-import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.hugorafaelcosta.chucknorrisjokeapp.databinding.ActivityMainBinding
+import okhttp3.Call
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    val URL = "https://api.icndb.com/jokes/random"
+    var okHttpClient: OkHttpClient = OkHttpClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.giveMeAJokeBtn.setOnClickListener {
+            loadRandomJoke()
+        }
+    }
 
-        setContentView(R.layout.activity_main)
+    private fun loadRandomJoke() {
+        runOnUiThread{
+            binding.progressBar.visibility = View.VISIBLE
+        }
+        val request: Request = Request.Builder().url(URL).build()
+        okHttpClient.newCall(request).enqueue(object : okhttp3.Callback {
+            override fun onFailure(call: Call, e: IOException) {
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val json = response.body?.string()
+                val txt = (JSONObject(json).getJSONObject("value")).get("joke").toString()
+
+                runOnUiThread {
+                    binding.progressBar.visibility = View.GONE
+                    binding.jokeTextView.text = Html.fromHtml(txt)
+                }
+
+            }
+        })
     }
 
 }
